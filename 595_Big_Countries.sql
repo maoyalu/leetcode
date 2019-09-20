e320a90787df52f3f90b04b5f4c129844dc2c3ab
Yalu <maoyalu27@gmail.com>
2019-09-20 23:14:03 +1000

    Solution for Q.627

--- 595_Big_Countries.sql
+++ 595_Big_Countries.sql
@@ -1,5 +1,5 @@
 /* ********** Attempt 1 - 2019/09/20  ********** */
 
-select name, population, area 
-from World
-where area > 3000000 or population > 25000000
+SELECT name, population, area 
+FROM World
+WHERE area > 3000000 OR population > 25000000
