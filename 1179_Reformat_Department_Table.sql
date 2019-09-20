b442570a8ede69d682c80298711e96019f477321
Yalu <maoyalu27@gmail.com>
2019-09-21 02:46:29 +1000

    Solution for Q.1179

--- 1179_Reformat_Department_Table.sql
+++ 1179_Reformat_Department_Table.sql
@@ -12,6 +12,6 @@ SELECT id,
     SUM(if(month ='Sep', revenue, null)) AS Sep_Revenue,
     SUM(if(month ='Oct', revenue, null)) AS Oct_Revenue,
     SUM(if(month ='Nov', revenue, null)) AS Nov_Revenue,
-    SUM(if(month ='Dec', revenue, null)) AS Dec_Revenue,
+    SUM(if(month ='Dec', revenue, null)) AS Dec_Revenue
 FROM Department
 GROUP BY ID
